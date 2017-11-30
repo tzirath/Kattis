ab7f6924624c8108fb61c3f8955a6ecf9a6f1b28
Tzirath  Perez <tzirath.perez@hotmail.com>
2017-11-30 22:50:28 +0000

    Add files via upload

---
+++ R2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class R2 {
+
+    public static void main(String args[])
+    {
+
+    	Scanner scan = new Scanner(System.in);
+
+    	int r1 = scan.nextInt();
+    	int s = scan.nextInt();
+
+    	int r2 = (s*2)-r1;
+    	System.out.println(r2);
+    }
+
+
+}
